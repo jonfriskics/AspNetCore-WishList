d956492d17db99fc9423b0116d7aa0a5265bdcbf
Jon Friskics <jonfriskics@gmail.com>
2018-05-21 23:53:59 -0400

    module 3 task 4 take 2

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WishList.Models
 {
     public class Item
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
         public Item()
         {
         }
